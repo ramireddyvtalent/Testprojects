96e3d61f0d95eadefc2ed33ffb89f5273233e5b4
ramireddyvtalent <ramireddy.vtalent@gmail.com>
2019-12-19 17:43:27 +0530

    BusinessAcess Add One Class

---
+++ Testprojects/Business Acess/Operation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Testprojects.Business_Acess
+{
+    public class Operation
+    {
+        public int Adition()
+        {
+            int a=10;
+            int b=20;
+            int c = a + b;
+            return c;
+
+        }
+    }
+}
